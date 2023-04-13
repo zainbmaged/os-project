28f3772e3baa12d4495f0d6e19dd1c81eb8fefeb
zainbmaged <1901420@eng.asu.edu.eg>
2023-04-13 21:01:31 +0200

    Create Output.java

---
+++ src/Output.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trail;
+
+import java.util.List;
+
+/**
+ *
+ * @author Eng. ZAINAB
+ */
+public class Output {
+     private final List<Process> processes;
+    private double avg_turnaround;
+    private double avg_waiting;
+
+    public Output(List<Process> processes, double avg_waiting, double avg_turnaround) {
+        this.processes = processes;
+        this.avg_waiting = avg_waiting;
+        this.avg_turnaround = avg_turnaround;
+    }
+
+    @Override
+    public String toString() {
+        return "Output{" + "\nprocesses=" + processes + "\navg_waiting=" + avg_waiting + "\navg_turnaround=" + avg_turnaround + "\n}";
+    }
+
+    public List<Process> getProcesses() {
+        return processes;
+    }
+
+    public double getAvg_turnaround() {
+        return avg_turnaround;
+    }
+
+    public double getAvg_waiting() {
+        return avg_waiting;
+    }
+
+}
