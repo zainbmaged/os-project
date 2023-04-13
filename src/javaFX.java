d71ec56e33f017698c5a80b4fcd9eca8f3700f0c
zainbmaged <1901420@eng.asu.edu.eg>
2023-04-13 20:59:09 +0200

    Create javaFX.java

---
+++ src/javaFX.java
@@ -0,0 +1,38 @@
+package trail;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Eng. ZAINAB
+ */
+public class javafx extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setTitle("CPU schadular");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
