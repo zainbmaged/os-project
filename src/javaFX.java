20868f8e7e1ce6d1d7a59f17069debe26cce2ee4
zainbmaged <1901420@eng.asu.edu.eg>
2023-04-14 22:40:37 +0200

    Update javaFX.java

--- src/javaFX.java
+++ src/javaFX.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
 package trail;
 
 import java.io.IOException;
