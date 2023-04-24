91f51921efa6765a5da49b119a865ce263ad7343
Sara Ramadan <87481838+Sara556M@users.noreply.github.com>
2023-04-24 20:05:14 +0200

    update_preemptivePriority_Class

---
+++ src/PreemptivePriority.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trail;
+
+/**
+ *
+ * @author sara
+ */
+
+
+import java.util.Comparator;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PreemptivePriority {
+    public static Output run(ArrayList<Process> processes) {
+        ArrayList<Process> executedProcesses = new ArrayList<>();
+        int currentTime = 0;
+        PriorityQueue<Process> queue = new PriorityQueue<>(Comparator.comparingInt(Process::getPriority));
+        ArrayList<Integer> waitingTimes = new ArrayList<>();
+        ArrayList<Integer> turnaroundTimes = new ArrayList<>();
+
+        while (!processes.isEmpty() || !queue.isEmpty()) {
+            // Add arrived processes to the queue
+            while (!processes.isEmpty() && processes.get(0).getArrival_time() <= currentTime) {
+                queue.add(processes.remove(0));
+            }
+
+            // Execute process with highest priority
+            if (!queue.isEmpty()) {
+                Process currentProcess = queue.poll();
+
+                // Record start time for the process
+                if (currentProcess.getStartTime() == 0) {
+                    currentProcess.setStartTime(currentTime);
+                }
+
+                // Execute process for 1 time unit
+                currentProcess.setRemainingBurstTime(currentProcess.getRemainingBurstTime() - 1);
+                currentTime++;
+
+                // Add process back to the queue if it still has remaining burst time
+                if (currentProcess.getRemainingBurstTime() > 0) {
+                    queue.add(currentProcess);
+                } else {
+                    // Record finish time for the process
+                    currentProcess.setFinishTime(currentTime);
+                    executedProcesses.add(currentProcess);
+
+                    // Record waiting time and turnaround time for the process
+                    int waitingTime = currentProcess.getFinishTime() - currentProcess.getBrust_time() - currentProcess.getChartarrival();
+                    waitingTimes.add(waitingTime);
+                    int turnaroundTime = currentProcess.getFinishTime() - currentProcess.getChartarrival();
+                    turnaroundTimes.add(turnaroundTime);
+                }
+            } else {
+                currentTime++;
+            }
+        }
+
+        // Calculate average waiting time and average turnaround time
+        double avgWaitingTime = waitingTimes.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+        double avgTurnaroundTime = turnaroundTimes.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+
+        return new Output(executedProcesses, avgWaitingTime, avgTurnaroundTime);
+    }
+
+
+   public static void main(String[] args) {
+    ArrayList<Process> processes = new ArrayList<>();
+    processes.add(new Process("p1", 0, 1, 2));
+    processes.add(new Process("p2", 1, 7, 6));
+    processes.add(new Process("p3", 2, 3, 3));
+    processes.add(new Process("p4", 3, 6, 5));
+    processes.add(new Process("p5", 4, 5, 4));
+    processes.add(new Process("p6", 5, 15, 10));
+    processes.add(new Process("p7", 6, 8, 9));
+    
+
+    Output output = PreemptivePriority.run(processes);
+
+    System.out.println(output);
+}
+
+}
