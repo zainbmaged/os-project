502277a798a9f24d3223aff4fd48948d9387d023
Menna-Elkhwass <105253678+Menna-Elkhwass@users.noreply.github.com>
2023-04-26 13:29:32 +0200

    Update priorityNonpreem.java

--- src/priorityNonpreem.java
+++ src/priorityNonpreem.java
@@ -5,58 +5,54 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class NonPreemptivePriority {
-    public static Output run(ArrayList<Process> processes) {
-        ArrayList<Process> executedProcesses = new ArrayList<>();
-        int currentTime = 0;
-        PriorityQueue<Process> queue = new PriorityQueue<>(Comparator.comparingInt(Process::getPriority));
-        ArrayList<Integer> waitingTimes = new ArrayList<>();
-        ArrayList<Integer> turnaroundTimes = new ArrayList<>();
+   public static Output run(ArrayList<Process> processes) {
+    ArrayList<Process> executedProcesses = new ArrayList<>();
+    int currentTime = 0;
+    PriorityQueue<Process> queue = new PriorityQueue<>(Comparator.comparingInt(Process::getPriority));
+    ArrayList<Integer> waitingTimes = new ArrayList<>();
+    ArrayList<Integer> turnaroundTimes = new ArrayList<>();
 
-        while (!processes.isEmpty() || !queue.isEmpty()) {
-            // Add arrived processes to the queue
-            while (!processes.isEmpty() && processes.get(0).getArrival_time() <= currentTime) {
-                queue.add(processes.remove(0));
-            }
-
-            // Execute process with highest priority
-            if (!queue.isEmpty()) {
-                Process currentProcess = queue.poll();
-
-                // Record start time for the process
-                if (currentProcess.getStartTime() == 0) {
-                    currentProcess.setStartTime(currentTime);
-                }
-
-                // Execute the process until it is completed
-                while (currentProcess.getRemainingBurstTime() > 0) {
-                    currentProcess.setRemainingBurstTime(currentProcess.getRemainingBurstTime() - 1);
-                    currentTime++;
-
-                    // Add arrived processes to the queue while the current process is executing
-                    while (!processes.isEmpty() && processes.get(0).getArrival_time() <= currentTime) {
-                        queue.add(processes.remove(0));
-                    }
-                }
-
-                // Record finish time for the process
-                currentProcess.setFinishTime(currentTime);
-                executedProcesses.add(currentProcess);
-
-                // Record waiting time and turnaround time for the process
-                int waitingTime = currentProcess.getFinishTime() - currentProcess.getBrust_time() - currentProcess.getChartarrival();
-                waitingTimes.add(waitingTime);
-                int turnaroundTime = currentProcess.getFinishTime() - currentProcess.getChartarrival();
-                turnaroundTimes.add(turnaroundTime);
-            } else {
-                currentTime++;
-            }
+    while (!processes.isEmpty() || !queue.isEmpty()) {
+        // Add arrived processes to the queue
+        while (!processes.isEmpty() && processes.get(0).getArrival_time() <= currentTime) {
+            queue.add(processes.remove(0));
         }
 
-        // Calculate average waiting time and average turnaround time
-        double avgWaitingTime = waitingTimes.stream().mapToInt(Integer::intValue).average().orElse(0.0);
-        double avgTurnaroundTime = turnaroundTimes.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+        // Execute process with highest priority
+        if (!queue.isEmpty()) {
+            Process currentProcess = queue.poll();
 
-        return new Output(executedProcesses, avgWaitingTime, avgTurnaroundTime);
+            // Record start time for the process
+            if (currentProcess.getStartTime() == 0) {
+                currentProcess.setStartTime(currentTime);
+            }
+
+            // Execute process until completion
+            currentTime += currentProcess.getRemainingBurstTime();
+            currentProcess.setFinishTime(currentTime);
+            executedProcesses.add(currentProcess);
+
+            // Record waiting time and turnaround time for the process
+            int waitingTime = currentProcess.getFinishTime() - currentProcess.getBrust_time() - currentProcess.getChartarrival();
+            waitingTimes.add(waitingTime);
+            int turnaroundTime = currentProcess.getFinishTime() - currentProcess.getChartarrival();
+            turnaroundTimes.add(turnaroundTime);
+        } else {
+            currentTime++;
+        }
+    }
+
+    // Set RemainingBurstTime of each process to its BurstTime
+    for (Process process : executedProcesses) {
+        process.setRemainingBurstTime(process.getBrust_time());
+    }
+
+    // Calculate average waiting time and average turnaround time
+    double avgWaitingTime = waitingTimes.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+    double avgTurnaroundTime = turnaroundTimes.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+
+    return new Output(executedProcesses, avgWaitingTime, avgTurnaroundTime);
+}
     }
 
     public static void main(String[] args) {
